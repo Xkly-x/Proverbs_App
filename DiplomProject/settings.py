fb1496d37a2cdf1a5636119e5066fdb5e9dcf424
Xkly-x <alisher_osmonov.com@inbox.ru>
2026-03-08 16:10:17 +0600

    Fix settings for real this time

--- DiplomProject/settings.py
+++ DiplomProject/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--e-g@457pb&57%egx_8ii#=cq%o8cq^m1ijq3u4ynumn$(jymh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Добавляем домен от Render
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['proverbs-app.onrender.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
