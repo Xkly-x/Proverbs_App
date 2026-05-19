a3e342662e2a4d9f104cd1033c443d9554abd79b
Xkly-x <alisher_osmonov.com@inbox.ru>
2026-05-19 12:20:46 +0600

    final

--- DiplomProject/settings.py
+++ DiplomProject/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'Static']
+STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 LOGOUT_REDIRECT_URL = 'login'
